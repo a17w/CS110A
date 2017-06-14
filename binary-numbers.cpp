bc2a3294ffeee48eb8a7c1e1f94ed48dfdb9d419
Annie Wong <annie.hs.wong@gmail.com>
2017-06-13 17:28:00 -0700

    added binary-numbers.cpp

---
+++ binary-numbers.cpp
@@ -0,0 +1,36 @@
+/* 2. Convert decimal numbers into binary numbers:
+a. 100 b. 1,000   c. 256   d. 27   e. 48   f. 112   g. 96   h. 1,024
+*/
+
+a. 1100100
+b. 1111101000
+c. 100000000
+d. 11011
+e. 110000
+f. 1110000
+g. 1100000
+h. 10000000000
+
+/*3. Convert binary numbers into decimal numbers
+a. 100	b. 101	c. 1100	d. 10101	e. 1111   f. 10000   g. 1111000   h. 110110
+*/
+
+a. 4
+b. 5
+c. 12
+d. 21
+e. 15
+f. 16
+g. 120
+h. 54
+
+/* 4. Describe what is wrong with this pattern of binary numbers:
+10 	 100    110    1000   1001	1100	1110	10000
+*/
+
+These binary numbers converts to even numbers of 2 except for 1001.
+(2, 4, 6, 8, 9, 12, 14, 16)
+
+/* 5. What is the decimal equivalent of  20? 21? 28?
+*/
+1, 2, 256
