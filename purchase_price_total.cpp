acc528cb511799d04409705ec3e10519a037468c
Annie Wong <annie.hs.wong@gmail.com>
2017-06-15 10:32:24 -0700

    completed part 2

--- purchase_price_total.cpp
+++ purchase_price_total.cpp
@@ -1,3 +1,4 @@
+/*
 #include <iostream>         // Needed to do C++ I/O
 #include <string>           // Needed by some compilers to use strings
 using namespace std;
@@ -22,4 +23,35 @@ total = subtotal + salesTax;
 cout << total << endl;
 
 return 0;
+}
+
+*/
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+double item1, item2, item3, item4, item5;
+double subtotal, salesTax, total;
+const double SALESTAXRATE = 0.06; // const must be capitalized
+
+cout << "What is the price of item 1?" << endl;
+cin >> item1;
+cout << "What is the price of item 2?" << endl;
+cin >> item2;
+cout << "What is the price of item 3?" << endl;
+cin >> item3;
+cout << "What is the price of item 4?" << endl;
+cin >> item4;
+cout << "What is the price of item 5?" << endl;
+cin >> item5;
+
+subtotal = item1 + item2 + item3 + item4 + item5;
+salesTax = subtotal * SALESTAXRATE;
+total = subtotal + salesTax;
+
+cout << "The total is " << total << endl;
+
+  return 0;
 }
