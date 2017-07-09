b99732b122281d0a9083741af3b236fdff0f0078
Annie Wong <annie.hs.wong@gmail.com>
2017-07-09 00:54:55 -0700

    completed lab_5.5

--- cookies.cpp
+++ cookies.cpp
@@ -24,10 +24,10 @@ int main()
         cin  >> numBoxes;
 
 // WRITE CODE TO START A while LOOP THAT LOOPS WHILE numBoxes IS NOT EQUAL TO -1, THE SENTINEL VALUE.
-        while (numBoxes != -1 )
+        while (numBoxes != -1)
         {
                 totalBoxes = totalBoxes + numBoxes; // WRITE CODE TO ADD numBoxes TO THE totalBoxes ACCUMULATOR.
-                numSeller++; // WRITE CODE TO ADD 1 TO THE numSeller COUNTER.
+                numSeller = numSeller + 1; // WRITE CODE TO ADD 1 TO THE numSeller COUNTER.
 
                 cout << "Enter number of boxes of cookies sold by the next seller: "; // WRITE CODE TO PROMPT FOR AND INPUT THE NUMBER OF BOXES SOLD BY THE NEXT SELLER.
                 cin >> numBoxes;
@@ -35,18 +35,20 @@ int main()
 
         numSeller = numSeller - 1; // WHEN THE LOOP IS EXITED, THE VALUE STORED IN THE numSeller COUNTER WILL BE ONE MORE THAN THE ACTUAL NUMBER OF SELLERS.SO WRITE CODE TO ADJUST IT TO THE ACTUAL NUMBER OF SELLERS.
 
-/* testing code to make sure loop + counter work
-        cout << "Total Boxes of Cookies Sold: " << totalBoxes << endl;
-        cout << "Number of Sellers: " << numSeller << endl;
- */
-        if (numSeller == 0)    // If true, -1 was the very first entry
+/* testing code to make sure loop + counter work */
+        // cout << "Total Boxes of Cookies Sold: " << totalBoxes << endl;
+        // cout << "Number of Sellers: " << numSeller << endl;
+
+
+        if (numSeller == 0)           // If true, -1 was the very first entry
                 cout << "\nNo boxes were sold.\n";
         else
-        {  // WRITE CODE TO ASSIGN averageBoxes THE COMPUTED AVERAGE NUMBER OF BOXES SOLD PER SELLER.
+        {         // WRITE CODE TO ASSIGN averageBoxes THE COMPUTED AVERAGE NUMBER OF BOXES SOLD PER SELLER.
                 averageBoxes = (static_cast<double>(totalBoxes) / (numSeller));
                 // WRITE CODE TO PRINT OUT THE NUMBER OF SELLERS AND AVERAGE NUMBER OF BOXES SOLD PER SELLER.
                 cout << "The average number of boxes sold by the " << numSeller << " sellers was " << averageBoxes << "." << endl;
         }
+
 
         return 0;
 }
